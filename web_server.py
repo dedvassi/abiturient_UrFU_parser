15a602cff8a8df69420f88301adf6e7405a94f0a
dedvassi <dedvassi@gmail.com>
2025-07-16 04:04:53 +0500

    добавлен веб сервер

---
+++ web_server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+import threading
+from telegram_bot import main as start_bot
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "🤖 Telegram-бот УрФУ работает!"
+
+def run_bot():
+    start_bot()
+
+if __name__ == '__main__':
+    threading.Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
